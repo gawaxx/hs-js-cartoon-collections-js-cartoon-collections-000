582e1550ea7f151b2d7b330ee09c9fba9d4c32c2
jules blanc <jules.blanc@unccub.com>
2019-09-25 19:50:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 function dwarfRollCall(dwarves) {
+  var Receiver = [];
+  for (var i = 0; i < dwarves.length; ++i){
+    Receiver.push((i+1) + '. '+  dwarves[i] + ' ');
+  }
+  return Receiver.join(''); 
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var Array1 = [];
+  
+  for (var i = 0; i < planeteerCalls.length; ++i) {
+    Array1.push(planeteerCalls[i].toUpperCase() + '!' );
+  }
+  
+  return Array1;
 }
 
 function longPlaneteerCalls(words) {
+  for (var i = 0; i < words.length; ++i) {
+    
+    if (words[i].length > 4) {return true;}
+    
+    else {return false;}
+    
+  }
 }
 
 function findTheCheese (foods) {
+  for (var i = 0; i < foods.length; ++i) {
+    
+    if (foods[i] === 'gouda'|| foods[i] === 'cheddar' || foods[i] === 'camembert' ) {
+      return foods[i];
+    }
+    
+  }
+  
+   return 'no cheese!';
 }
